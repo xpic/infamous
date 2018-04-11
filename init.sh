5f883829966e93bbf849ef5acb580473b05d4b39
Xavier Pic <xavier.pic.2@outlook.fr>
2018-04-11 08:19:36 +0200

    Adding tensorflow virtualenv configuration file

--- init.sh
+++ init.sh
@@ -1,5 +1,12 @@
+echo "Setting the password for the root"
+passwd root
+adduser guest
+echo "Setting the password for the guest account"
+read pswd
+passwd guest 
+echo "Guest password initialized to $pswd"
+cp .bash_alias .bash_export .bash_history .bash_login .bash_logout .bash_profile .bashrc .emacs .gitignore .gitconfig .logo .logo2 .logo3 .zshrc ~Tensorflow ~/
 apt-get install -y make cmake screenfetch tree ssh python pyton-pip subversion git emacs24 telegram-cli fish zsh g++ gcc gdb openjdk-8-jdk vim 
 pip install sklearn matplotlib jupyter scipy virtualenv
-adduser guest
-cp .bash_alias .bash_export .bash_history .bash_login .bash_logout .bash_profile .bashrc .emacs .gitignore .gitconfig .logo .logo2 .logo3 .zshrc ~Tensorflow ~/
 cp sudoers /etc/sudoers #Attention Dangereux configurer le mot de passe du root avant
+#Packages
