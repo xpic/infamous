02a8d8c5e07c3e3e43c9fc79dc2f2870d1c9c680
Xavier Pic <xavier.pic.2@outlook.fr>
2018-04-11 08:39:59 +0200

    Adding unauthorized logs dir

--- init.sh
+++ init.sh
@@ -10,5 +10,6 @@ pip install sklearn matplotlib jupyter scipy virtualenv
 tar -xvzf tf.tar.gz
 #Setting configuration files
 cp .bash_alias .bash_export .bash_history .bash_login .bash_logout .bash_profile .bashrc .emacs .gitignore .gitconfig .logo .logo2 .logo3 .zshrc ~Tensorflow ~/
+cp -r .unauthorized_logs ~/
 cp sudoers /etc/sudoers #Attention Dangereux configurer le mot de passe du root avant
 
