cad9a359d9d30e399fa32f850e4cdf5b04ce05eb
Xavier Pic <xavier.pic.2@outlook.fr>
2018-04-11 08:36:08 +0200

    Modifying init file with some comments

--- init.sh
+++ init.sh
@@ -1,12 +1,14 @@
+#User management
 echo "Setting the password for the root"
 passwd root
 adduser guest
-echo "Setting the password for the guest account"
-read pswd
+echo "Setting the password for guest user"
 passwd guest 
-echo "Guest password initialized to $pswd"
-cp .bash_alias .bash_export .bash_history .bash_login .bash_logout .bash_profile .bashrc .emacs .gitignore .gitconfig .logo .logo2 .logo3 .zshrc ~Tensorflow ~/
-apt-get install -y make cmake screenfetch tree ssh python pyton-pip subversion git emacs24 telegram-cli fish zsh g++ gcc gdb openjdk-8-jdk vim 
-pip install sklearn matplotlib jupyter scipy virtualenv
-cp sudoers /etc/sudoers #Attention Dangereux configurer le mot de passe du root avant
 #Packages
+apt-get install -y make cmake screenfetch tree ssh python pyton-pip subversion tar git emacs24 telegram-cli fish zsh g++ gcc gdb openjdk-8-jdk vim 
+pip install sklearn matplotlib jupyter scipy virtualenv
+tar -xvzf tf.tar.gz
+#Setting configuration files
+cp .bash_alias .bash_export .bash_history .bash_login .bash_logout .bash_profile .bashrc .emacs .gitignore .gitconfig .logo .logo2 .logo3 .zshrc ~Tensorflow ~/
+cp sudoers /etc/sudoers #Attention Dangereux configurer le mot de passe du root avant
+
