80921331c9776d1b4c0f0009687e5b7ab3a65522
Xavier Pic <xavier.pic.2@outlook.fr>
2018-04-11 07:28:27 +0200

    Ajout git config files

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 apt-get install make cmake screenfetch tree ssh python pyton-pip subversion git emacs24 telegram-cli 
 pip install sklearn matplotlib jupyter scipy keras
 adduser guest
+mv .bashrc .emacs .gitignore .gitconfig .zshrc ~/
